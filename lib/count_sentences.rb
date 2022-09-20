3debfbb8d58d7ff9e28a199dbcae453e018b207d
Pete Fowler <fowler.pete@gmail.com>
2022-09-20 10:39:19 -0600

    Finish exercise

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,14 +11,26 @@ class String
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    arr = self.split(/\.|\?|!/)
+    arr.select! do |i| 
+      i != ''
+    end 
+    arr.length
   end
 end
